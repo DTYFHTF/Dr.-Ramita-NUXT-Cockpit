9e6c2f48a8e1b9f7d1fbfcade13a3ff560b26b8c
Abin Maharjan <clashwithabin@gmail.com>
2025-08-07 11:53:29 +0530

    removed categories-demo from the navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -66,12 +66,6 @@
           <li class="nav-item">
             <a class="nav-link" href="/event">Events</a>
           </li>
-          <li class="nav-item">
-            <NuxtLink to="/categories-demo" class="nav-link text-primary fw-bold">
-              <LucideIcon icon="mdi:file-tree" class="me-1" />
-              Category Demo
-            </NuxtLink>
-          </li>
         </ul>
         <!-- User Section -->
         <div
